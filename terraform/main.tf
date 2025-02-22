6bae74c7d7be058977fa75546d6110378a63cf98
cvmahesh <rush2cvmahesh@gmail.com>
2025-02-23 01:05:45 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Look up an existing security group by name
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["web-security-group"]
+  }
+}
+
+
 # Security Group allowing HTTP (8080) and SSH (22)
 resource "aws_security_group" "web_sg" {
+  count       = length(data.aws_security_group.existing_sg.id) > 0 ? 0 : 1
   name        = "web-security-group"
   description = "Allow HTTP on 8080 and SSH"
 
@@ -49,7 +59,10 @@ resource "aws_instance" "web_server" {
   ami             = "ami-05b10e08d247fb927"
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.deployer_key.key_name
-  security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = [
+    length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.new_sg[0].id
+  ]
+  #security_groups = [aws_security_group.web_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
